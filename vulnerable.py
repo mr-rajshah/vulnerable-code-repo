325644fea0cecaf335dca03b18c17fe626029acf
Raj Shah <68182166+mr-rajshah@users.noreply.github.com>
2025-02-02 12:45:51 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -1,7 +1,6 @@
 import os
 
 password = "hardcoded_secret"  # Hardcoded password (Critical vulnerability)
-password = "another_secret"  # New hardcoded secret
 
 def insecure_function():
     os.system("rm -rf /")  # Command Injection (Critical vulnerability)
